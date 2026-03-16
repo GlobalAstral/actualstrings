bcaef0b5f3fe524e4cae095898786271e57d41f2
GlobalAstral <cristianmainenti.03@gmail.com>
2026-03-16 19:35:21 +0100

    Hotfix

--- actualstrings.c
+++ actualstrings.c
@@ -3,7 +3,7 @@
 #define long long long
 #define BASE_CAP 64
 #define ceildiv(a, b) (((a) + (b) - 1) / (b))
-#define INTEGRITY_FLAG 0xFEEBDAEDEBABEFACCAFEBABEDEADBEEF
+#define INTEGRITY_FLAG 0xCAFEBABEDEADBEEFFEEBDAEDEBABEFAC
 
 DynamicString ds_from(char* init) {
   size_t sz = strlen(init);
@@ -29,7 +29,7 @@ bool check_ds(DynamicString* ds) {
   return ds->__integrity == INTEGRITY_FLAG;
 }
 
-Result ds_reserve(DynamicString* ds, size_t size) {
+AS_Result ds_reserve(DynamicString* ds, size_t size) {
   char* new_ptr = (char*)realloc(ds->__internal, size * sizeof(char));
   if (new_ptr == NULL) {
     return AS_CANNOT_ALLOCATE;
@@ -39,8 +39,8 @@ Result ds_reserve(DynamicString* ds, size_t size) {
   return AS_SUCCESS;
 }
 
-Result ds_append(DynamicString* ds, const char* str) {
-  if (!checkDynamicArray(ds)) {
+AS_Result ds_append(DynamicString* ds, const char* str) {
+  if (!check_ds(ds)) {
     return AS_NOT_SANE;
   }
   if (str == NULL) {
@@ -53,7 +53,7 @@ Result ds_append(DynamicString* ds, const char* str) {
     while (ds->length + str_size + 1 > factor * ds->__capacity) {
       factor *= 2;
     }
-    Result r = ds_reserve(ds, factor * ds->__capacity);
+    AS_Result r = ds_reserve(ds, factor * ds->__capacity);
     if (r) {
       return r;
     }
--- actualstrings.h
+++ actualstrings.h
@@ -8,14 +8,14 @@
 
 #define long long long
 
-typedef enum Result {
+typedef enum AS_Result {
   AS_SUCCESS,
   AS_NOT_SANE,
   AS_CANNOT_ALLOCATE,
   AS_OUT_OF_BOUNDS,
   AS_EMPTY,
   AS_BAD_ARG,
-} Result;
+} AS_Result;
 
 typedef struct DynamicString {
   char* __internal;
@@ -27,7 +27,7 @@ typedef struct DynamicString {
 DynamicString ds_from(char* init);
 DynamicString ds_new();
 bool check_ds(DynamicString* ds);
-Result ds_reserve(DynamicString* ds, size_t size);
-Result ds_append(DynamicString* ds, const char* str);
+AS_Result ds_reserve(DynamicString* ds, size_t size);
+AS_Result ds_append(DynamicString* ds, const char* str);
 
 #undef long
