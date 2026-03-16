66ae5ef07a7137a60c79ff0206ebe2ff7d674db8
GlobalAstral <cristianmainenti.03@gmail.com>
2026-03-16 14:24:20 +0100

    first commit

---
+++ actualstrings.c
@@ -0,0 +1,67 @@
+#include "actualstrings.h"
+
+#define long long long
+#define BASE_CAP 64
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define INTEGRITY_FLAG 0xFEEBDAEDEBABEFACCAFEBABEDEADBEEF
+
+DynamicString ds_from(char* init) {
+  size_t sz = strlen(init);
+  size_t needed_cap = ceildiv(sz - 1, BASE_CAP);
+  return (DynamicString) {
+    .__capacity = needed_cap,
+    .length = sz,
+    .__internal = (char*)malloc(needed_cap*sizeof(char)),
+    .__integrity = INTEGRITY_FLAG,
+  };
+}
+
+DynamicString ds_new() {
+  return (DynamicString) {
+    .__capacity = BASE_CAP,
+    .length = 0,
+    .__internal = (char*)malloc(BASE_CAP*sizeof(char)),
+    .__integrity = INTEGRITY_FLAG,
+  };
+}
+
+bool check_ds(DynamicString* ds) {
+  return ds->__integrity == INTEGRITY_FLAG;
+}
+
+Result ds_reserve(DynamicString* ds, size_t size) {
+  char* new_ptr = (char*)realloc(ds->__internal, size * sizeof(char));
+  if (new_ptr == NULL) {
+    return AS_CANNOT_ALLOCATE;
+  }
+  ds->__internal = new_ptr;
+  ds->__capacity = size;
+  return AS_SUCCESS;
+}
+
+Result ds_append(DynamicString* ds, const char* str) {
+  if (!checkDynamicArray(ds)) {
+    return AS_NOT_SANE;
+  }
+  if (str == NULL) {
+    return AS_BAD_ARG;
+  }
+  size_t str_size = strlen(str);
+
+  if (ds->length + str_size + 1 > ds->__capacity) {
+    size_t factor = 2;
+    while (ds->length + str_size + 1 > factor * ds->__capacity) {
+      factor *= 2;
+    }
+    Result r = ds_reserve(ds, factor * ds->__capacity);
+    if (r) {
+      return r;
+    }
+  }
+
+  memcpy(ds->__internal + ds->length, str, str_size);
+  ds->length += str_size;
+  ds->__internal[ds->length] = 0;
+
+  return AS_SUCCESS;
+}
---
+++ actualstrings.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define str_of_dyn(dynptr) (dynptr->__internal)
+
+#define long long long
+
+typedef enum Result {
+  AS_SUCCESS,
+  AS_NOT_SANE,
+  AS_CANNOT_ALLOCATE,
+  AS_OUT_OF_BOUNDS,
+  AS_EMPTY,
+  AS_BAD_ARG,
+} Result;
+
+typedef struct DynamicString {
+  char* __internal;
+  size_t __capacity;
+  size_t length;
+  unsigned long __integrity;
+} DynamicString;
+
+DynamicString ds_from(char* init);
+DynamicString ds_new();
+bool check_ds(DynamicString* ds);
+Result ds_reserve(DynamicString* ds, size_t size);
+Result ds_append(DynamicString* ds, const char* str);
+
+#undef long
