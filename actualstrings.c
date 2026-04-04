dec61b728b2fe7cb3d3226c3bec6961cd1368b92
GlobalAstral <cristianmainenti.03@gmail.com>
2026-04-04 11:10:55 +0200

    Bugfix

--- actualstrings.c
+++ actualstrings.c
@@ -7,7 +7,7 @@
 
 DynamicString ds_from(char* init) {
   size_t sz = strlen(init);
-  size_t needed_cap = ceildiv(sz - 1, BASE_CAP);
+  size_t needed_cap = ceildiv(sz, BASE_CAP) * BASE_CAP;
   char* buf = (char*)malloc(needed_cap*sizeof(char));
   memcpy(buf, init, sz);
   buf[sz] = 0;
